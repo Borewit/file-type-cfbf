2925771d71d8f5c62ef77b58db6b06c8040965bf
Borewit <Borewit@users.noreply.github.com>
2025-12-23 20:05:46 +0100

    Empty detection plugin library for CFBF

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+import type { ITokenizer } from 'strtok3';
+import type { Detector, FileTypeResult } from 'file-type';
+
+export const detectCfbf: Detector = {
+	id: 'xml',
+	detect: async (tokenizer: ITokenizer):  Promise<FileTypeResult | undefined> => {
+		// To implement:
+		// https://github.com/sindresorhus/file-type/pull/223#issuecomment-3668477211
+	}
+};
---
+++ test/test.js
@@ -0,0 +1,75 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {readFile} from 'node:fs/promises';
+import {describe, it} from 'mocha';
+import {fromFile} from 'strtok3';
+import {assert} from 'chai';
+
+import {detectCfbf} from '../lib/index.js';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+function getSamplePath(filename) {
+	return path.join(dirname, 'fixture', filename);
+}
+
+describe('CFBF detector', () => {
+
+	it('should return undefined on any other file', async () => {
+		const samplePath = getSamplePath('other.txt');
+		const tokenizer = await fromFile(samplePath);
+		try {
+			const fileType = await detectCfbf.detect(tokenizer);
+			assert.isUndefined(fileType);
+			assert.strictEqual(tokenizer.position, 0, 'position should be be advanced');
+		} finally {
+			await tokenizer.close();
+		}
+
+	});
+
+	it('should detect .doc', async () => {
+		const samplePath = getSamplePath('sample1.doc');
+		const tokenizer = await fromFile(samplePath);
+		try {
+			const fileType = await detectCfbf.detect(tokenizer);
+			assert.isDefined(fileType, 'should detect the file type');
+			assert.strictEqual(fileType.mime, 'application/msword');
+			assert.strictEqual(fileType.ext, 'doc');
+		} finally {
+			await tokenizer.close();
+		}
+
+	});
+
+	it('should detect .xls', async () => {
+		const samplePath = getSamplePath('sample1.xls');
+		const tokenizer = await fromFile(samplePath);
+		try {
+			const fileType = await detectCfbf.detect(tokenizer);
+			assert.isDefined(fileType, 'should detect the file type');
+			assert.strictEqual(fileType.mime, 'application/vnd.ms-excel');
+			assert.strictEqual(fileType.ext, 'xls');
+		} finally {
+			await tokenizer.close();
+		}
+
+	});
+
+	it('should detect .ppt', async () => {
+		const samplePath = getSamplePath('sample1.ppt');
+		const tokenizer = await fromFile(samplePath);
+		try {
+			const fileType = await detectCfbf.detect(tokenizer);
+			assert.isDefined(fileType, 'should detect the file type');
+			assert.strictEqual(fileType.mime, 'application/vnd.ms-powerpoint');
+			assert.strictEqual(fileType.ext, 'ppt');
+		} finally {
+			await tokenizer.close();
+		}
+
+	});
+
+});
+
